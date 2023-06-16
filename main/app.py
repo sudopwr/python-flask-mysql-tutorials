0fd0ede4a2d1929dddd93065ebef67199562668c
sudopwr <sudopwr@gmail.com>
2023-06-16 18:08:05 +0530

    feat: add db connection

--- main/app.py
+++ main/app.py
@@ -1,8 +1,31 @@
-"""Flask App main module"""
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost:3306/dukaan'
+db = SQLAlchemy(app)
 
-@app.route('/')
-def index():
-    return 'Hello, World!'
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+with app.app_context():
+    admin = User('admin', 'admin@example.com')
+    
+    db.create_all() # In case user table doesn't exists already. Else remove it.    
+    
+    db.session.add(admin)
+    
+    db.session.commit() # This is needed to write the changes to database
+    
+    User.query.all()
+    
+    User.query.filter_by(username='admin').first()
