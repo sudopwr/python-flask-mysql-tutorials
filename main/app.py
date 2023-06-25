85f595c0ee6c7b43574be446044f6b465fbbe8c7
sudopwr <sudopwr@gmail.com>
2023-06-25 23:44:37 +0530

    style: format code

--- main/app.py
+++ main/app.py
@@ -4,14 +4,13 @@ from flask_sqlalchemy import SQLAlchemy
 
 from main.extensions import db
 from main.config import config
-from main.usecases.products import products_api_bpl 
 from main.usecases import register_route
+from main.models import *
 
 app = Flask(__name__)
 app.config.from_object(config.get(os.getenv("APP_MODE")))
 db.init_app(app)
 register_route(app)
-from main.models import *
 
 with app.app_context():
     db.create_all()
