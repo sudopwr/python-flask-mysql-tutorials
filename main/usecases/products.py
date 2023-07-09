f05863abf394b42eb07a1c669d2ca4c9bd47f1fa
sudopwr <sudopwr@gmail.com>
2023-07-10 00:49:30 +0530

    feat: add product api

--- main/usecases/products.py
+++ main/usecases/products.py
@@ -1,7 +1,18 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from main.models import Product
+from main.extensions import db
 
 products_api_bpl = Blueprint("Products", __name__)
 
-@products_api_bpl.route("/products")
-def home():
-    return "hello"
+
+@products_api_bpl.route("/products", methods=["post"])
+def add_product():
+    try:
+        request_json = request.get_json()
+        product = Product(
+            name=request_json["name"], price=request_json["price"], description=request_json["description"], quantity=request_json["quantity"], image='test.jpg')
+        db.session.add(product)
+        db.session.commit()
+        return '', 201
+    except:
+        return {"message": "Failed to add product"}, 400
