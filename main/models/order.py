5d4085df448b67528ea22cad5d059a20ec4f55db
sudopwr <sudopwr@gmail.com>
2023-07-16 22:00:13 +0530

    feat: update order module add quantity status date

--- main/models/order.py
+++ main/models/order.py
@@ -1,13 +1,15 @@
 from main.extensions import db
+from dataclasses import dataclass
+import datetime
 
+@dataclass
 class Order(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
-    user_id = db.Column(db.String(80), db.ForeignKey('user.id'), nullable=False)
-
-    def __init__(self, product_id, user_id):
-        self.product_id = product_id
-        self.user_id = user_id
+    id: int = db.Column(db.Integer, primary_key=True)
+    product_id: int = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    user_id: int = db.Column(db.String(80), db.ForeignKey('user.id'), nullable=False)
+    quantity: int = db.Column(db.Integer, nullable=False)
+    status: str = db.Column(db.String(100), nullable=False)
+    order_date = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
 
     def __repr__(self):
         return '<Orders %r>' % self.id
