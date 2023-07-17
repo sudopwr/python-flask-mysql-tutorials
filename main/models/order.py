39c8d257e5542ad00bfb41e6ac559051f6e49e46
sudopwr <sudopwr@gmail.com>
2023-07-17 20:48:21 +0530

    feat: return order records

--- main/models/order.py
+++ main/models/order.py
@@ -9,7 +9,7 @@ class Order(db.Model):
     user_id: int = db.Column(db.String(80), db.ForeignKey('user.id'), nullable=False)
     quantity: int = db.Column(db.Integer, nullable=False)
     status: str = db.Column(db.String(100), nullable=False)
-    order_date = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
+    order_date: str = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
 
     def __repr__(self):
         return '<Orders %r>' % self.id
--- main/usecases/orders.py
+++ main/usecases/orders.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify, g
-from main.models import Order
+from main.models import Order, User, Product
 from main.extensions import db
 from main.utils import login_required, should_admin
 
@@ -36,9 +36,27 @@ def get_order():
 @login_required
 def get_users_orders():
     try:
-        order = Order.query.filter(Order.user_id == g.user["id"]).all()
-        print(order)
-        return jsonify(order), 200
+        orders = Order.query\
+            .join(User, Order.user_id == User.id)\
+            .join(Product, Order.product_id == Product.id)\
+            .add_columns(User.name, Product.name)\
+            .filter(Order.user_id == g.user["id"]).all()
+        result = []
+        for order in orders:
+            _order = order[0]
+            _order = {
+                "id": _order.id,
+                "product_id": _order.product_id,
+                "user_id": _order.user_id,
+                "quantity": _order.quantity,
+                "status": _order.status,
+                "order_date": _order.order_date,
+                "user_name": order[1],
+                "product_name": order[2]
+            }
+            result.append(_order)
+
+        return jsonify(result), 200
     except Exception as e:
         print(e)
         return {"message": "Failed to get order"}, 400
